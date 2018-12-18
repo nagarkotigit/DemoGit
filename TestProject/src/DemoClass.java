811b2bfaf65f6d6a5708df0c2b2113aa156b554f
nagarkotigit <nagarkotiacme@gmail.com>
2018-12-18 11:38:04 +0530

    New content Added -himanshu

--- TestProject/src/DemoClass.java
+++ TestProject/src/DemoClass.java
@@ -2,7 +2,27 @@
 public class DemoClass {
 
 	
-	public static void main(String[] args) {
-		System.out.println("Hi Java");
+	
+		// Function to return the sum of all the integers 
+		// below N which are multiples of either A or B 
+		static int findSum(int n, int a, int b) 
+		{ 
+		    int sum = 0; 
+		    for (int i = 0; i < n; i++) 
+		  
+		        // If i is a multiple of a or b 
+		        if (i % a == 0 || i % b == 0) 
+		            sum += i; 
+		  
+		    return sum; 
+		} 
+		  
+		// Driver code 
+		  
+		  
+		    public static void main (String[] args) { 
+		        int n = 10, a = 3, b = 5; 
+		    System.out.println( findSum(n, a, b)); 
+		    } 
 	}
-}
+
